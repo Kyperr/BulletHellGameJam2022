be8ce7b95f119f2196c960d78ff982f9d709bda0
Kyper <danielmcbride550@hotmail.com>
2022-04-17 10:55:21 -0700

    More enemy behavior.

--- Assets/Enemies/Instances/Binary Templar/BinaryTemplar.cs
+++ Assets/Enemies/Instances/Binary Templar/BinaryTemplar.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BinaryTemplar : MonoBehaviour
 {
+    public enum Phase
+    {
+        CIRCLING,
+        FOCUSSING
+    }
 
     private const float DISTANCE_THRESHOLD = 1.0f;
 
@@ -17,27 +22,53 @@ public class BinaryTemplar : MonoBehaviour
     private float circlingAngle = 30f;
 
     [SerializeField]
+    private Vector2 circlingTimeRange = new Vector2(3, 10);
+
+    // When the AI is done circling, this is how long they pause to look at the player
+    [SerializeField]
+    private Vector2 focusOnPlayerTimeRange = new Vector2(5, 8);
+
+    [SerializeField]
     private float moveSpeed = 10;
 
     [SerializeField]
     private float circlingSpeed = 30;
 
+    private Phase phase = Phase.CIRCLING;
+
     private bool rotating = true;
 
     private float desiredAngle = 0;
+
+    [SerializeField]
+    private float timeSpentOnPhase = 0;
+
+    private float timeToCircle = 0;
 
 
     void Start()
     {
         desiredAngle = Random.Range(0, 360);
+        timeSpentOnPhase = 0;
+        phase = Phase.CIRCLING;
+        timeToCircle = Random.Range(circlingTimeRange.x, circlingTimeRange.y);
     }
 
     void Update()
     {
+        timeSpentOnPhase += Time.deltaTime;
 
-        UpdateDesiredAngle();
+        if (phase == Phase.CIRCLING)
+        {
+            Debug.Log("CIRCLING");
+            CirclingLogic();
+        }
 
-        GetToDesiredDistance();
+        if (phase == Phase.FOCUSSING)
+        {
+            Debug.Log("FOCUSSING");
+            FocussingLogic();
+        }
 
         // 1) Enemy should circle the player for N seconds
         // 2) After N seconds, stop, turn and face player.
@@ -45,12 +76,46 @@ public class BinaryTemplar : MonoBehaviour
         // 4) Continue circling the player, randomly choose clockwise or counter clockwise.
     }
 
+    private void CirclingLogic()
+    {
+        // The core of the logic
+        UpdateDesiredAngle();
+        MoveToDesiredPosition();
+
+        // Is it time to swap phases?
+        if (timeSpentOnPhase >= timeToCircle)
+        {
+            Debug.Log("timeSpentOnPhase >= timeToCircle: " + timeSpentOnPhase + " >= " + timeToCircle);
+            SwapToPhase(Phase.FOCUSSING);
+            timeToCircle = Random.Range(focusOnPlayerTimeRange.x, focusOnPlayerTimeRange.y);
+        }
+    }
+
+    private void FocussingLogic()
+    {
+
+        transform.LookAt(target.transform.position, Vector3.up);
+
+        // Is it time to swap phases?
+        if (timeSpentOnPhase >= timeToCircle)
+        {
+            SwapToPhase(Phase.CIRCLING);
+            timeToCircle = Random.Range(circlingTimeRange.x, circlingTimeRange.y);
+        }
+    }
+
+    private void SwapToPhase(Phase phase)
+    {
+        this.phase = phase;
+        timeSpentOnPhase = 0;
+    }
+
     private void UpdateDesiredAngle()
     {
         desiredAngle += Time.deltaTime * circlingSpeed;
     }
 
-    private void GetToDesiredDistance()
+    private void MoveToDesiredPosition()
     {
         Vector3 directionalVector = new Vector3(Mathf.Cos(Mathf.Deg2Rad * desiredAngle), 0, Mathf.Sin(Mathf.Deg2Rad * desiredAngle));
         Vector3 desiredPosition = this.target.transform.position + (directionalVector * desiredDistance);
