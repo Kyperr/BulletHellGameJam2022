884ccde124a136969c2a4fc5af5212c0c7d9893d
Kyper <danielmcbride550@hotmail.com>
2022-04-17 10:37:41 -0700

    Some behavior for first enemy.

--- Assets/Enemies/Instances/Binary Templar/BinaryTemplar.cs
+++ Assets/Enemies/Instances/Binary Templar/BinaryTemplar.cs
@@ -5,16 +5,59 @@ using UnityEngine;
 public class BinaryTemplar : MonoBehaviour
 {
 
+    private const float DISTANCE_THRESHOLD = 1.0f;
+
     [SerializeField]
     private GameObject target;
 
-    // Update is called once per frame
+    [SerializeField]
+    private float desiredDistance = 10f;
+
+    [SerializeField]
+    private float circlingAngle = 30f;
+
+    [SerializeField]
+    private float moveSpeed = 10;
+
+    [SerializeField]
+    private float circlingSpeed = 30;
+
+    private bool rotating = true;
+
+    private float desiredAngle = 0;
+
+
+    void Start()
+    {
+        desiredAngle = Random.Range(0, 360);
+    }
+
     void Update()
     {
+
+        UpdateDesiredAngle();
+
+        GetToDesiredDistance();
+
         // 1) Enemy should circle the player for N seconds
         // 2) After N seconds, stop, turn and face player.
         // 3) Shoot bullets towards player and wait for X amount of seconds (configurable)
         // 4) Continue circling the player, randomly choose clockwise or counter clockwise.
+    }
+
+    private void UpdateDesiredAngle()
+    {
+        desiredAngle += Time.deltaTime * circlingSpeed;
+    }
+
+    private void GetToDesiredDistance()
+    {
+        Vector3 directionalVector = new Vector3(Mathf.Cos(Mathf.Deg2Rad * desiredAngle), 0, Mathf.Sin(Mathf.Deg2Rad * desiredAngle));
+        Vector3 desiredPosition = this.target.transform.position + (directionalVector * desiredDistance);
+
+        transform.LookAt(desiredPosition, Vector3.up);
+
+        this.transform.position = Vector3.Lerp(this.transform.position, desiredPosition, Time.deltaTime * moveSpeed);
     }
 
 }
